83d2966802cb34c17e8e439c7d312a06dc80f3d7
Jnayeh <mouradslim44@gmail.com>
2026-01-15 11:46:19 +0100

    initializes qatra project

---
+++ donation-service/src/main/java/com/zayenha/qatra/QatraApplication.java
@@ -0,0 +1,13 @@
+package com.zayenha.qatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QatraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QatraApplication.class, args);
+    }
+
+}
---
+++ donation-service/src/test/java/com/zayenha/qatra/QatraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayenha.qatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QatraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
