f9f8b53e5558f8f444ba96348d4a518193a511c1
Jnayeh <mouradslim44@gmail.com>
2026-02-08 23:15:23 +0100

    refactor: update CenterRepositoryPort with shared SearchCriteria

--- donation-service/src/main/java/com/zayenha/qatra/center/domain/port/out/CenterRepositoryPort.java
+++ donation-service/src/main/java/com/zayenha/qatra/center/domain/port/out/CenterRepositoryPort.java
@@ -1,6 +1,8 @@
 package com.zayenha.qatra.center.domain.port.out;
 
 import com.zayenha.qatra.center.domain.model.DonationCenter;
+import com.zayenha.qatra.shared.domain.PageResult;
+import com.zayenha.qatra.shared.domain.SearchCriteria;
 
 import java.util.Optional;
 
@@ -8,4 +10,5 @@ public interface CenterRepositoryPort {
     DonationCenter save(DonationCenter center);
     boolean existsByName(String name);
     Optional<DonationCenter> findById(Long id);
+    PageResult<DonationCenter> findAll(SearchCriteria criteria);
 }
