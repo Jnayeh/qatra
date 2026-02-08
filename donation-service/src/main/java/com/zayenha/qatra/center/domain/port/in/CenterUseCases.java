2aec582981150d35a4a471d2d97f1dcf131d564b
Jnayeh <mouradslim44@gmail.com>
2026-02-08 21:37:21 +0100

    refactor: rename CenterCommandUseCases to CenterUseCases

--- donation-service/src/main/java/com/zayenha/qatra/center/domain/port/in/CenterUseCases.java
+++ donation-service/src/main/java/com/zayenha/qatra/center/domain/port/in/CenterUseCases.java
@@ -1,11 +1,15 @@
 package com.zayenha.qatra.center.domain.port.in;
 
 import com.zayenha.qatra.center.domain.model.DonationCenter;
+import com.zayenha.qatra.shared.domain.SearchCriteria;
 import com.zayenha.qatra.center.domain.model.FacilityType;
 import com.zayenha.qatra.center.domain.model.OperatingHours;
+import com.zayenha.qatra.shared.domain.PageResult;
 
-public interface CenterCommandUseCases {
+public interface CenterUseCases {
     DonationCenter create(CreateCenterCommand command);
+    DonationCenter getById(Long id);
+    PageResult<DonationCenter> getAll(SearchCriteria criteria);
 
     record CreateCenterCommand(
         String name,
