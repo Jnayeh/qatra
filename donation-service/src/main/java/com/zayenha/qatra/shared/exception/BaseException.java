dbc03d23520a39ee0bf5c25d00f51de5f6f89bc8
Jnayeh <mouradslim44@gmail.com>
2026-02-09 13:38:36 +0100

    refactor: add @Getter and lazy data init to BaseException

--- donation-service/src/main/java/com/zayenha/qatra/shared/exception/BaseException.java
+++ donation-service/src/main/java/com/zayenha/qatra/shared/exception/BaseException.java
@@ -1,26 +1,26 @@
 package com.zayenha.qatra.shared.exception;
 
+import lombok.Getter;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
 public abstract class BaseException extends RuntimeException {
     private final String errorCode;
     private final int httpStatus;
-    private final Map<String, Object> data;
+    private Map<String, Object> data;
 
     public BaseException(String message, String errorCode, int httpStatus) {
         super(message);
         this.errorCode = errorCode;
         this.httpStatus = httpStatus;
-        this.data = new HashMap<>();
     }
 
-    public String getErrorCode() { return errorCode; }
-    public int getHttpStatus() { return httpStatus; }
-    public Map<String, Object> getData() { return data; }
-
-    public BaseException addData(String key, Object value) {
+    public void addData(String key, Object value) {
+        if (data == null) {
+            data = new HashMap<>();
+        }
         data.put(key, value);
-        return this;
     }
 }
