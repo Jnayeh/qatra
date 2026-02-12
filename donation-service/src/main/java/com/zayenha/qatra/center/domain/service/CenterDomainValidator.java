593f98802e8cac6147b08ca72eb54b6120c797f9
Jnayeh <mouradslim44@gmail.com>
2026-02-12 18:54:58 +0100

    refactor: add validateUpdate to CenterDomainValidator

--- donation-service/src/main/java/com/zayenha/qatra/center/domain/service/CenterDomainValidator.java
+++ donation-service/src/main/java/com/zayenha/qatra/center/domain/service/CenterDomainValidator.java
@@ -3,6 +3,7 @@ package com.zayenha.qatra.center.domain.service;
 import com.zayenha.qatra.center.domain.exception.CenterErrorCode;
 import com.zayenha.qatra.center.domain.port.out.CenterRepositoryPort;
 import com.zayenha.qatra.shared.exception.ConflictException;
+import com.zayenha.qatra.shared.exception.NotFoundException;
 
 public class CenterDomainValidator {
 
@@ -15,6 +16,13 @@ public class CenterDomainValidator {
     public void validateCreate(String name) {
         if (centerRepository.existsByName(name)) {
             throw new ConflictException("Center name already exists: " + name,
+                    CenterErrorCode.CENTER_NAME_ALREADY_EXISTS.name());
+        }
+    }
+
+    public void validateUpdate(Long id, String name) {
+        if (centerRepository.otherCenterHasName(id, name)) {
+            throw new ConflictException("Other center already named: " + name,
                     CenterErrorCode.CENTER_NAME_ALREADY_EXISTS.name());
         }
     }
