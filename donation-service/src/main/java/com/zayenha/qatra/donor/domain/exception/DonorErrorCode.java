559c9f5d9f4dc833587dd50034c58ad3dd2d65dd
Jnayeh <mouradslim44@gmail.com>
2026-02-19 18:45:51 +0100

    feat: add DonorErrorCode enum Error codes: DONOR_NOT_FOUND, BLOOD_TYPE_ALREADY_VERIFIED, HEALTH_QUESTIONNAIRE_NOT_FOUND, DONOR_ALREADY_EXISTS

---
+++ donation-service/src/main/java/com/zayenha/qatra/donor/domain/exception/DonorErrorCode.java
@@ -0,0 +1,8 @@
+package com.zayenha.qatra.donor.domain.exception;
+
+public enum DonorErrorCode {
+    DONOR_NOT_FOUND,
+    BLOOD_TYPE_ALREADY_VERIFIED,
+    HEALTH_QUESTIONNAIRE_NOT_FOUND,
+    DONOR_ALREADY_EXISTS
+}
