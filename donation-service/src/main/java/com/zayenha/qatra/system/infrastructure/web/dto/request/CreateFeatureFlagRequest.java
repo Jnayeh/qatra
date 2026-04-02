f1ad6db06e85610091c4618344cb7baed0c59204
Jnayeh <mouradslim44@gmail.com>
2026-04-02 19:29:06 +0100

    refactor: remove SystemConfig and FeatureFlag modules (YAGNI)

--- donation-service/src/main/java/com/zayenha/qatra/system/infrastructure/web/dto/request/CreateFeatureFlagRequest.java
+++
@@ -1,9 +0,0 @@
-package com.zayenha.qatra.system.infrastructure.web.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CreateFeatureFlagRequest(
-    @NotBlank String flagName,
-    boolean enabled,
-    String description
-) {}
--- donation-service/src/main/java/com/zayenha/qatra/system/infrastructure/web/dto/request/SetConfigRequest.java
+++
@@ -1,9 +0,0 @@
-package com.zayenha.qatra.system.infrastructure.web.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record SetConfigRequest(
-    @NotBlank String key,
-    @NotBlank String value,
-    String description
-) {}
