c959c71257a90209e28572f86b5dc968c0ec81ad
Jnayeh <mouradslim44@gmail.com>
2026-04-15 20:19:52 +0100

    feat: add NGINX, Prometheus, Grafana, and Postgres configs - nginx/nginx.conf with REST + WebSocket routing to both services - prometheus/prometheus.yml scraping /actuator/prometheus on both services - grafana/datasources/datasource.yml auto-provisioned Prometheus datasource - postgres/init.sql multi-database initialization (qatra + qatra_notification)

---
+++ postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE USER qatra WITH PASSWORD 'qatra';
+CREATE DATABASE qatra OWNER qatra;
+
+CREATE USER qatra_notification WITH PASSWORD 'qatra_notification';
+CREATE DATABASE qatra_notification OWNER qatra_notification;
