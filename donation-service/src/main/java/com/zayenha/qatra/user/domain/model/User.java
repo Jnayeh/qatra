730bbbeed385d67b2b258eb145c56d857a56c0ab
MouradSlimJnayeh <mouradslim44@gmail.com>
2026-07-20 14:48:00 +0100

    fix: fixes signup without dedicated username

--- donation-service/src/main/java/com/zayenha/qatra/user/domain/model/User.java
+++ donation-service/src/main/java/com/zayenha/qatra/user/domain/model/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -29,7 +30,7 @@ public class User {
         this.email = email;
         this.phone = phone;
         this.hashedPassword = hashedPassword;
-        this.displayName = displayName;
+        this.displayName = Optional.ofNullable(displayName).orElse(firstName.concat(" ").concat(familyName));
         this.firstName = firstName;
         this.familyName = familyName;
         this.status = UserStatus.PENDING_VERIFICATION;
